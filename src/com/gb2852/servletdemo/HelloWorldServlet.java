b2d5c2b3ea06ed71d365946cd5307ca5dbda1684
Sherin-Shaji <61889833+Sherin-Shaji@users.noreply.github.com>
2020-03-10 13:46:29 -0400

    Update HelloWorldServlet.java

--- src/com/gb2852/servletdemo/HelloWorldServlet.java
+++ src/com/gb2852/servletdemo/HelloWorldServlet.java
@@ -1,6 +1,8 @@
 package com.gb2852.servletdemo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,19 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Step 1: set the content type
+		response.setContentType("text/html");
+		
+		// Step 2: get the printwriter
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: generate HTML content
+		out.println("<html><body>");
+		
+		out.println("<h2>Hello World</h2>");
+		out.println("<hr>");
+		out.println("Time on the server is: " + new java.util.Date());
+		out.println("</body></html>");
 	}
 
 	/**
